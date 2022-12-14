deb36054a193c4a4c72149ee6f35c9a691116d39
MohamedOsman15 <osman.mohamed2244@gmail.com>
2022-12-14 13:55:53 -0600

    added comments to post on feed

--- instaclone/src/componenents/Feed.jsx
+++ instaclone/src/componenents/Feed.jsx
@@ -5,12 +5,18 @@ import { BASE_URL } from "../services/api";
 const Feed = () => {
     const [posts, updatePosts] = useState([])
     const [comments, updateComments] = useState([])
+    const [users, updateUsers] = useState([])
 
     useEffect(() => {
         const api = async () => {
             let res = await axios.get(`${BASE_URL}api/posts`)
             updatePosts(res.data)
+            let res2 = await axios.get(`${BASE_URL}api/comments`)
+            updateComments(res2.data)
+            let res3 = await axios.get(`${BASE_URL}api/users`)
+            updateUsers(res3.data)
         }
+        
         api()
     }, [])
 
@@ -22,7 +28,26 @@ const Feed = () => {
                     <div className="post" key={res.key}>
                         <img src={res.image}/>
                         <p>{res.caption}</p>
-                        <p>comments</p>
+                        {comments.map((comment) => {
+                            for (let i = 0; i < comments.length; i++) {
+                                if (comment.postId === res.id ) {
+                                    return (
+                                        <div>
+                                            {users.map((user) => {
+                                                for (let i = 0; i < users.length; i++) {
+                                                    if (user.id === comment.userId) {
+                                                        return (
+                                                            <p>{user.displayName}:</p>
+                                                        )
+                                                    }
+                                                }
+                                            })}
+                                            <p>{comment.comment}</p>
+                                        </div>
+                                    )
+                                }
+                            }
+                        })}
                     </div>
                 )
             })}
