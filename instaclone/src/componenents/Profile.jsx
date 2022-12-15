f8f4eac0950a96b9ee06d9a6901dbb3e209bbe57
MohamedOsman15 <osman.mohamed2244@gmail.com>
2022-12-14 23:41:37 -0600

    added card to profile page

--- instaclone/src/componenents/Profile.jsx
+++ instaclone/src/componenents/Profile.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
+import Avatar from '@mui/joy/Avatar';
+import { Box } from "@mui/system";
 import { useEffect, useState } from "react";
 import { BASE_URL } from "../services/api";
 import { useParams } from "react-router-dom";
+import InstagramPost from "./InstagramCard";
 
 const Profile = () => {
     const [posts, setPosts] = useState([])
@@ -23,15 +26,41 @@ const Profile = () => {
     return (
         <div className="profilePage">
             <div>
+                <div className="profileName">
+                <Box sx={{ display: 'flex', alignItems: 'center', pb: 1.5, gap: 1 }}>
+
+                <Box
+            sx={{
+                position: 'relative',
+                '&:before': {
+                    content: '""',
+                    position: 'absolute',
+                    top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              m: '-2px',
+              borderRadius: '50%',
+              background:
+              'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
+            },
+        }}
+        >
+          <Avatar
+            size="lg"
+            src="/static/logo.png"
+            sx={{ p: 0.5, border: '2px solid', borderColor: 'background.body' }}
+            />
+        </Box>
                 <h2 className="user">{user}</h2>
+                    </Box>
+                </div>
+              
                 <div className="posts">
                 {posts.map((res) => {
                     return (
-                        <div className="post" key={res.id}>
-                        <img src={res.image}/>
-                        <p>{res.caption}</p>
-                    </div>
-                )})}    
+                        <InstagramPost image={res.image} user={user.displayName} userId={userId} caption={res.caption} postId={res.id}/>
+                        )})}    
                 </div>
             </div>
         </div>
